6ac981b0bdb1320285c3caf315f5a7df68242bfc
baseballproject1 <ryuhj0829@gmail.com>
2026-07-24 18:14:00 +0900

    Update teams.js

--- teams.js
+++ teams.js
@@ -1,172 +1,104 @@
-// =================================
-// KBO 카드게임 - 구단 데이터
-// =================================
+
+// =====================================
+// KBO 카드게임 팀 데이터
+// =====================================
 
 
-const teams = {
+const teams = [
 
-
-    SSG: {
-
+    {
+        id:"SSG",
         name:"SSG 랜더스",
-
-        stadium:"인천 SSG 랜더스필드",
-
-        color:"#e4002b",
-
-        legends:[
-            "양현종"
-        ]
-
+        money:10000000000,
+        legendCount:5
     },
 
 
-    LG: {
-
-        name:"LG 트윈스",
-
-        stadium:"서울 잠실야구장",
-
-        color:"#c30452",
-
-        legends:[
-
-        ]
-
-    },
-
-
-    KIA: {
-
+    {
+        id:"KIA",
         name:"KIA 타이거즈",
-
-        stadium:"광주-기아 챔피언스 필드",
-
-        color:"#ea0029",
-
-        legends:[
-
-        ]
-
+        money:10000000000,
+        legendCount:5
     },
 
 
-    KT: {
-
+    {
+        id:"KT",
         name:"KT 위즈",
-
-        stadium:"수원 KT 위즈파크",
-
-        color:"#000000",
-
-        legends:[
-
-        ]
-
+        money:10000000000,
+        legendCount:5
     },
 
 
-    삼성: {
-
+    {
+        id:"삼성",
         name:"삼성 라이온즈",
-
-        stadium:"대구 삼성 라이온즈파크",
-
-        color:"#074ca1",
-
-        legends:[
-
-        ]
-
+        money:10000000000,
+        legendCount:5
     },
 
 
-    두산: {
-
+    {
+        id:"두산",
         name:"두산 베어스",
-
-        stadium:"서울 잠실야구장",
-
-        color:"#131230",
-
-        legends:[
-
-        ]
-
+        money:10000000000,
+        legendCount:5
     },
 
 
-    롯데: {
-
+    {
+        id:"롯데",
         name:"롯데 자이언츠",
-
-        stadium:"부산 사직야구장",
-
-        color:"#041e42",
-
-        legends:[
-
-        ]
-
+        money:10000000000,
+        legendCount:5
     },
 
 
-    한화: {
+    {
+        id:"LG",
+        name:"LG 트윈스",
+        money:10000000000,
+        legendCount:5
+    },
 
+
+    {
+        id:"한화",
         name:"한화 이글스",
-
-        stadium:"대전 한화생명 볼파크",
-
-        color:"#ff6600",
-
-        legends:[
-
-        ]
-
+        money:10000000000,
+        legendCount:5
     },
 
 
-    키움: {
-
+    {
+        id:"키움",
         name:"키움 히어로즈",
-
-        stadium:"서울 고척스카이돔",
-
-        color:"#570514",
-
-        legends:[
-
-        ]
-
+        money:10000000000,
+        legendCount:5
     },
 
 
-    NC: {
-
+    {
+        id:"NC",
         name:"NC 다이노스",
-
-        stadium:"창원 NC파크",
-
-        color:"#315288",
-
-        legends:[
-
-        ]
-
+        money:10000000000,
+        legendCount:5
     }
 
-
-};
-
+];
 
 
 
-// 팀 선택 함수
-
-function getTeam(teamCode){
 
 
-    return teams[teamCode];
+// 팀 찾기
+
+function getTeam(teamId){
+
+
+    return teams.find(
+        team=>team.id===teamId
+    );
 
 
 }
